e1cf25aa5bbbc5d7a2e9d86530bb5dd7b23b8a9d
Sieper.fabian@outlook.com <Sieper.fabian@outlook.com>
2025-12-08 23:33:15 +0100

    Add angular frontend files

---
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [],
+  template: ` <h1>Welcome to NotionQuest!</h1> `,
+  styleUrl: './landing-page.component.scss',
+})
+export class LandingPageComponent {}
